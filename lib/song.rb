50b977eae25cd4fda38086746cb83bc4323ac00a
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-26 02:48:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,9 +42,9 @@ class Song
 
   def self.new_from_filename(filename)
     song = self.new
-    parse = filename.split("-",".")
-    song.artist_name = parse[0].to_s
-    song.name = parse[1].to_s
+    parse = filename.split("-")
+    song.artist_name = parse[0]
+    song.name = parse[1]
 
   end
 
